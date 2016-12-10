2ef82bbff637f628559e3c0b225ef5258d2a182b
Frank Matranga <thefrankmatranga@gmail.com>
2016-12-10 17:08:02 -0500

    Very useful permission utility

---
+++ lib/regit/utilities.rb
@@ -0,0 +1,28 @@
+module Regit
+  module Utilities
+    def self.list_to_perms(perms)
+      allow = Discordrb::Permissions.new
+      deny = Discordrb::Permissions.new
+      
+      perms.each do |p|
+        if p.start_with? '-'
+          # Deny
+          begin
+            deny.send("can_#{p.gsub('-', '')}=", true)
+          rescue NoMethodError => e
+            LOGGER.info "Discordrb can't handle #{p} yet. Skipping."
+          end
+        else
+          # Allow
+          begin
+            allow.send("can_#{p}=", true)
+          rescue NoMethodError => e
+            LOGGER.info "Discordrb can't handle #{p} yet. Skipping."
+          end
+        end
+      end
+
+      { allow: allow, deny: deny }
+    end
+  end
+end
