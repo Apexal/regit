cd354923edc255bcdd9ed8da956db03e4b289753
Frank Matranga <thefrankmatranga@gmail.com>
2017-01-07 23:05:34 -0500

    Track users

--- lib/regit/events/presence.rb
+++ lib/regit/events/presence.rb
@@ -5,6 +5,8 @@ module Regit
 
       presence do |event|
         LOGGER.info "#{event.user.distinct} is now #{event.user.status} on #{event.server.name}"
+
+        begin;Regit::Database::Student.find_by_discord_id(event.user.id).update(last_online: Time.new) if event.user.status == :offline;rescue;end
       end
     end
   end
