f520b2b8c8ac0bb96970eb64b538938eb4db4c35
Frank Matranga <thefrankmatranga@gmail.com>
2017-01-09 19:59:42 -0500

    Join/leave DMs

--- lib/regit/events/presence.rb
+++ lib/regit/events/presence.rb
@@ -3,6 +3,16 @@ module Regit
     module Presence
       extend Discordrb::EventContainer
 
+      member_join do |event|
+        LOGGER.info "#{event.user.distinct} HAS JOINED SERVER #{event.server.name}"
+        event.server.owner.pm "#{event.user.mention} HAS JOINED THE SERVER"
+      end
+
+      member_leave do |event|
+        LOGGER.info "#{event.user.distinct} HAS LEFT SERVER #{event.server.name}"
+        event.server.owner.pm "#{event.user.mention} HAS LEFT THE SERVER"
+      end
+
       presence do |event|
         LOGGER.info "#{event.user.distinct} is now #{event.user.status} on #{event.server.name}"
 
