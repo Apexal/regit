445068558947fc230351e2ac38d488e60ac58fd2
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-01 14:02:42 -0400

    Removed school part of member setup

--- lib/regit/registration.rb
+++ lib/regit/registration.rb
@@ -4,7 +4,7 @@ module Regit
   module Registration
     extend StoreData
 
-    UNALLOWED = ['Advisement', 'Health', 'Guidance', 'Phys Ed']
+    # UNALLOWED = ['Advisement', 'Health', 'Guidance', 'Phys Ed']
 
     VERIFY_CODES = load_file("#{Dir.pwd}/data/verify_codes.yaml")
     # {
@@ -48,95 +48,95 @@ module Regit
       Regit::Database::Group.where(owner_username: student.username).update_all(owner_username: 'fmatranga18') # Nasty, I know
     end
 
-    def self.handle_advisement_system(member)
-      LOGGER.info "Setting up advisement stuff for #{member.distinct} | #{member.info.description}"
-      server = member.server
-      student = member.info
+    # def self.handle_advisement_system(member)
+    #   LOGGER.info "Setting up advisement stuff for #{member.distinct} | #{member.info.description}"
+    #   server = member.server
+    #   student = member.info
 
-      large_adv = student.advisement[0..1]
-      small_adv = student.advisement
+    #   large_adv = student.advisement[0..1]
+    #   small_adv = student.advisement
 
-      # Since seniors are in 1 advisement
-      advs = student.grade == 12 ? [large_adv] : [large_adv, small_adv]
+    #   # Since seniors are in 1 advisement
+    #   advs = student.grade == 12 ? [large_adv] : [large_adv, small_adv]
 
-      # Remove other advisement roles
-      other_roles = server.roles.select { |r| !advs.include?(r.name) && Regit::Database::Student.distinct.pluck(:advisement).include?(r.name) }
+    #   # Remove other advisement roles
+    #   other_roles = server.roles.select { |r| !advs.include?(r.name) && Regit::Database::Student.distinct.pluck(:advisement).include?(r.name) }
 
-      new_roles = []
+    #   new_roles = []
 
-      perms = Discordrb::Permissions.new
-      perms.can_read_messages = true
-      perms.can_send_messages = true
-      perms.can_read_message_history = true
-      perms.can_mention_everyone = true
+    #   perms = Discordrb::Permissions.new
+    #   perms.can_read_messages = true
+    #   perms.can_send_messages = true
+    #   perms.can_read_message_history = true
+    #   perms.can_mention_everyone = true
 
-      advs.each do |a|
-        adv_role = server.roles.find { |r| r.name == a }
-        if adv_role.nil?
-          # Need to create role
-          adv_role = server.create_role
-          adv_role.name = a
-          adv_role.hoist = true if a.length <= 2 # If large advisement only
-          adv_role.mentionable = true
-        end
-        new_roles << adv_role
+    #   advs.each do |a|
+    #     adv_role = server.roles.find { |r| r.name == a }
+    #     if adv_role.nil?
+    #       # Need to create role
+    #       adv_role = server.create_role
+    #       adv_role.name = a
+    #       adv_role.hoist = true if a.length <= 2 # If large advisement only
+    #       adv_role.mentionable = true
+    #     end
+    #     new_roles << adv_role
 
-        adv_channel = server.text_channels.find { |t| t.name == a.downcase }
-        if adv_channel.nil?
-          adv_channel = server.create_channel(a, 0)
-          adv_channel.topic = "Private chat for **Advisement #{a}**."
+    #     adv_channel = server.text_channels.find { |t| t.name == a.downcase }
+    #     if adv_channel.nil?
+    #       adv_channel = server.create_channel(a, 0)
+    #       adv_channel.topic = "Private chat for **Advisement #{a}**."
 
-          pos = server.text_channels.find { |t| t.name == 'seniors' }.position
-          adv_channel.position = pos
+    #       pos = server.text_channels.find { |t| t.name == 'seniors' }.position
+    #       adv_channel.position = pos
 
-          adv_channel.define_overwrite(adv_role, perms, 0) # Advisement members
-          adv_channel.define_overwrite(server.roles.find { |r| r.id == server.id }, 0, perms) # @everyone
-        end
-      end
+    #       adv_channel.define_overwrite(adv_role, perms, 0) # Advisement members
+    #       adv_channel.define_overwrite(server.roles.find { |r| r.id == server.id }, 0, perms) # @everyone
+    #     end
+    #   end
 
-      member.modify_roles(new_roles, other_roles)
-    end
+    #   member.modify_roles(new_roles, other_roles)
+    # end
 
-    def self.handle_course_channels(member)
-      # Course channels
-      LOGGER.info "Handling course channels for #{member.distinct} | #{member.info.description}"
-      courses = member.info.classes
-      courses.each { |c| handle_course_channel(member.server, c, member) }
-    end
+    # def self.handle_course_channels(member)
+    #   # Course channels
+    #   LOGGER.info "Handling course channels for #{member.distinct} | #{member.info.description}"
+    #   courses = member.info.classes
+    #   courses.each { |c| handle_course_channel(member.server, c, member) }
+    # end
 
-    def self.course_name(full_name)
-      full_name = full_name.split(' (')[0].split(' ').join('-')
-      full_name.gsub!(/\W+/, '-')
-      %w(IV III II I 9 10 11 12).each { |i| full_name.gsub!("-#{i}", '') }
+    # def self.course_name(full_name)
+    #   full_name = full_name.split(' (')[0].split(' ').join('-')
+    #   full_name.gsub!(/\W+/, '-')
+    #   %w(IV III II I 9 10 11 12).each { |i| full_name.gsub!("-#{i}", '') }
 
-      full_name
-    end
+    #   full_name
+    # end
 
-    def self.handle_course_channel(server, course, user=nil)
-      return if UNALLOWED.any? { |w| course.title.include?(w) }
-      LOGGER.info "Handling course channel for #{course.title}"
-      text_channel = course.text_channel
+    # def self.handle_course_channel(server, course, user=nil)
+    #   return if UNALLOWED.any? { |w| course.title.include?(w) }
+    #   LOGGER.info "Handling course channel for #{course.title}"
+    #   text_channel = course.text_channel
 
-      perms = Discordrb::Permissions.new
-      perms.can_read_messages = true
-      perms.can_send_messages = true
-      perms.can_read_message_history = true
-      perms.can_mention_everyone = true
+    #   perms = Discordrb::Permissions.new
+    #   perms.can_read_messages = true
+    #   perms.can_send_messages = true
+    #   perms.can_read_message_history = true
+    #   perms.can_mention_everyone = true
 
-      # Create text-channel if not exist
-      if text_channel.nil?
-        text_channel = server.create_channel(course_name(course.title), 0)
-        text_channel.topic = "Discussion room for **#{course.title}** with **#{course.teacher.last_name}**. | http://moodle.regis.org/course/view.php?id=#{course.id}"
-        text_channel.send_message("Course Page: http://moodle.regis.org/course/view.php?id=#{course.id}").pin
-        text_channel.define_overwrite(server.roles.find { |r| r.id == server.id }, 0, perms)
-        course.update(text_channel_id: text_channel.id)
-      end
-      text_channel.define_overwrite(user, perms, 0) unless user.nil?
+    #   # Create text-channel if not exist
+    #   if text_channel.nil?
+    #     text_channel = server.create_channel(course_name(course.title), 0)
+    #     text_channel.topic = "Discussion room for **#{course.title}** with **#{course.teacher.last_name}**. | http://moodle.regis.org/course/view.php?id=#{course.id}"
+    #     text_channel.send_message("Course Page: http://moodle.regis.org/course/view.php?id=#{course.id}").pin
+    #     text_channel.define_overwrite(server.roles.find { |r| r.id == server.id }, 0, perms)
+    #     course.update(text_channel_id: text_channel.id)
+    #   end
+    #   text_channel.define_overwrite(user, perms, 0) unless user.nil?
 
-      # Save in DB
-      # Return text-channel
-      text_channel
-    end
+    #   # Save in DB
+    #   # Return text-channel
+    #   text_channel
+    # end
 
     # After verifying, setup a student
     def self.setup_user(member)
@@ -149,7 +149,7 @@ module Regit
       new_roles << server.roles.find { |r| r.name == 'Students' }
 
       # Add grade role
-      new_roles << server.roles.find { |r| r.name == member.info.grade_name }
+      # new_roles << server.roles.find { |r| r.name == member.info.grade_name }
 
       # Default groups
       Regit::Database::Group.where(default_group: true).each do |g|
@@ -168,10 +168,10 @@ module Regit
 
       member.modify_roles(new_roles, [])
 
-      unless Regit::School::summer?(server.school)
-        handle_advisement_system(member) rescue nil
-        handle_course_channels(member) rescue nil
-      end
+      # unless Regit::School::summer?(server.school)
+      #   handle_advisement_system(member) rescue nil
+      #   handle_course_channels(member) rescue nil
+      # end
 
       VERIFY_CODES.delete(member.info.username)
       LOGGER.info 'Done!'
